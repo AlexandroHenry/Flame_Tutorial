e961df96909f2ff1498971315a4857286fe5ee4f
Seungchul Ha <seungchulha@Seungchuls-MacBook-Pro.local>
2023-05-02 18:26:51 +0900

    3-character-movement

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyGame extends FlameGame {
   SpriteComponent cat2 = SpriteComponent();
   SpriteComponent background = SpriteComponent();
 
-  final double characterSize = 200;
+  final double characterSize = 100;
+
+  bool turnAway = false;
 
   @override
   Future<void> onLoad() async {
@@ -21,6 +23,8 @@ class MyGame extends FlameGame {
     final screenWidth = size[0];
     final screenHeight = size[1];
     const textBoxHeight = 200;
+
+    print('screenHeight: $screenHeight // screenWidth: $screenWidth');
 
     // load background
     add(background
@@ -33,15 +37,44 @@ class MyGame extends FlameGame {
     cat1
       ..sprite = await loadSprite('cat1.png')
       ..size = Vector2(characterSize, characterSize)
-      ..y = screenHeight - characterSize - textBoxHeight;
+      ..y = screenHeight - characterSize
+      ..x = characterSize;
+    // ..anchor = Anchor.bottomCenter;
     add(cat1);
 
     // load cat2
     cat2
+      // sprite 설정
       ..sprite = await loadSprite('cat2.png')
+      // 크기 설정
       ..size = Vector2(characterSize, characterSize)
-      ..y = screenHeight - characterSize - textBoxHeight
-      ..x = screenWidth - characterSize;
+      // Y 위치 설정 (화면 맨 아래)
+      ..y = screenHeight - characterSize
+      // X 위치 설정 (화면 맨 오른쪽)
+      ..x = screenWidth / 2 + characterSize
+      // Anchor 설정 (오른쪽 위)
+      // ..anchor = Anchor.bottomCenter
+      // 좌우 반전
+      ..flipHorizontally();
+
     add(cat2);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    if (cat1.x < size[1] / 2) {
+      cat1.y -= 50 * dt;
+      // cat1.x -= 1 * dt;
+    } else if (turnAway = false) {
+      print('turn away');
+      cat2.flipHorizontally();
+      turnAway = true;
+    }
+
+    if (cat2.x < size[1] / 2 - 100) {
+      cat2.y -= 5 * dt;
+    }
   }
 }
